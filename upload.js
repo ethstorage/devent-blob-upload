61249f2e70753d542da1c14fb98b40a885e63112
iteye <iteyelmp@gmail.com>
2024-02-06 10:59:12 +0800

    fix crash

--- upload.js
+++ upload.js
@@ -1,3 +1,6 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
 const {ethers, Contract} = require("ethers");
 const {BlobUploader, EncodeBlobs} = require("ethstorage-sdk");
 const crypto = require('crypto');
@@ -9,15 +12,18 @@ const contractABI = [
     "function putBlobs(uint256 num) public payable",
     "function lastKvIdx() public view returns (uint40)"
 ]
-const MAX_BLOB = 500000n;
+const MAX_BLOB = 2000000n;
+const privateKey = process.env.pk;
 
 let firstBlob = false;
 
-const send4844Tx = new BlobUploader(RPC, '');
-const provider = new ethers.JsonRpcProvider(RPC);
-const contract = new Contract(contractAddress, contractABI, provider);
+const sleep = (ms) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
 
-async function upload() {
+async function upload(contract) {
     let price = await contract.upfrontPayment();
     if (firstBlob) {
         price = price + BigInt(1000000000000);
@@ -31,12 +37,13 @@ async function upload() {
         gasLimit
     });
 
+    const send4844Tx = new BlobUploader(RPC, privateKey);
     // limit gas
     const fee = await send4844Tx.getFee();
     let maxFeePerGas = BigInt(fee.maxFeePerGas) * BigInt(6) / BigInt(5);
     let maxPriorityFeePerGas = BigInt(fee.maxPriorityFeePerGas) * BigInt(6) / BigInt(5);
-    if(maxFeePerGas > 2000000000n) {
-        maxFeePerGas = 2000000000n;
+    if(maxFeePerGas > 5000000000n) {
+        maxFeePerGas = 5000000000n;
     }
     let blobGas = await send4844Tx.getBlobGasPrice();
     blobGas = blobGas * 6n / 5n;
@@ -46,10 +53,10 @@ async function upload() {
     tx.maxFeePerGas = maxFeePerGas;
     tx.maxPriorityFeePerGas = maxPriorityFeePerGas;
     tx.maxFeePerBlobGas = blobGas;
-    // tx.nonce = 2;
-    // tx.maxFeePerGas = maxFeePerGas;
-    // tx.maxPriorityFeePerGas = maxPriorityFeePerGas;
-    // tx.maxFeePerBlobGas = 2100000000000n;
+    // tx.nonce = 5280;
+    // tx.maxFeePerGas = 24000000000n;
+    // tx.maxPriorityFeePerGas = maxPriorityFeePerGas * 3n;
+    // tx.maxFeePerBlobGas = 25000000000n * 2n;
 
     //  send
     const content = crypto.randomBytes(4096 * 31);
@@ -57,23 +64,26 @@ async function upload() {
     const hash = await send4844Tx.sendTx(tx, blobs);
     console.log(hash);
     const txReceipt = await send4844Tx.getTxReceipt(hash);
-    console.log(txReceipt.blockNumber, " | ", txReceipt.gasUsed)
+    console.log(`blockNumber=${txReceipt.blockNumber} gasUsed=${txReceipt.gasUsed}`)
     return txReceipt;
 }
 
 async function batchBlob() {
     while (true) {
-        const currentIndex = await contract.lastKvIdx();
-        const totalCount = MAX_BLOB - currentIndex;
-        console.log(currentIndex, totalCount);
-        if (totalCount <= 0) {
-            return;
-        }
-
         try {
-            await upload();
+            const provider = new ethers.JsonRpcProvider(RPC);
+            const contract = new Contract(contractAddress, contractABI, provider);
+            const currentIndex = await contract.lastKvIdx();
+            const totalCount = MAX_BLOB - currentIndex;
+            console.log("Current Number:",currentIndex, " Total Number:",totalCount);
+            if (totalCount <= 0) {
+                return;
+            }
+
+            await upload(contract);
         } catch (e) {
             console.log(e)
+            await sleep(3000);
         }
     }
 }
