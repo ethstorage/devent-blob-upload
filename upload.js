860d38141e59ae731e2b079074c6805e13a1b0ff
iteye <iteyelmp@gmail.com>
2024-06-28 16:27:33 +0800

    support l2

--- upload.js
+++ upload.js
@@ -5,19 +5,19 @@ const crypto = require('crypto');
 const dotenv = require("dotenv")
 dotenv.config()
 
-const RPC = "https://polished-silent-market.ethereum-sepolia.quiknode.pro/3b74a592be57773068d83f931dd98af8cbc1e9ca";
-const contractAddress = '0x804C520d3c084C805E37A35E90057Ac32831F96f'
+const RPC = "http://142.132.154.16:8545";
+const contractAddress = '0x580F6F360E78293510652e4355EEa39257A4a662'
 const contractABI = [
     "function upfrontPayment() public view returns (uint256)",
     "function putBlobs(uint256 num) public payable",
     "function lastKvIdx() public view returns (uint40)"
 ]
-const MAX_BLOB = 2000000n;
+const MAX_BLOB = 4000000n;
 
 let firstBlob = false;
 
 const privateKey = process.env.pk;
-const send4844Tx = new BlobUploader(RPC, privateKey);
+const blobUploader = new BlobUploader(RPC, privateKey);
 const provider = new ethers.JsonRpcProvider(RPC);
 const contract = new Contract(contractAddress, contractABI, provider);
 
@@ -34,40 +34,19 @@ async function upload() {
         firstBlob = false;
     }
 
-    let count = 250;
-    let gasLimit = 17000000;
+    let count = 300n;
+    let gasLimit = 19968224;
     const tx = await contract.putBlobs.populateTransaction(count, {
-        value: price,
+        value: price * count,
         gasLimit
     });
-
-    // limit gas
-    const fee = await send4844Tx.getFee();
-    let maxFeePerGas = BigInt(fee.maxFeePerGas) * BigInt(6) / BigInt(5);
-    let maxPriorityFeePerGas = BigInt(fee.maxPriorityFeePerGas) * BigInt(6) / BigInt(5);
-    if (maxFeePerGas > 5000000000n) {
-        maxFeePerGas = 5000000000n;
-    }
-    let blobGas = await send4844Tx.getBlobGasPrice();
-    blobGas = blobGas * 6n / 5n;
-    if (blobGas > 25000000000n) {
-        blobGas = 25000000000n;
-    }
-    tx.maxFeePerGas = maxFeePerGas;
-    tx.maxPriorityFeePerGas = maxPriorityFeePerGas;
-    tx.maxFeePerBlobGas = blobGas;
-    // tx.nonce = 5280;
-    // tx.maxFeePerGas = 24000000000n;
-    // tx.maxPriorityFeePerGas = maxPriorityFeePerGas * 3n;
-    // tx.maxFeePerBlobGas = 25000000000n * 2n;
-
     //  send
     const content = crypto.randomBytes(4096 * 31);
     const blobs = EncodeBlobs(content);
-    const hash = await send4844Tx.sendTx(tx, blobs);
-    console.log(hash);
-    const txReceipt = await send4844Tx.getTxReceipt(hash);
-    console.log(`blockNumber=${txReceipt.blockNumber} gasUsed=${txReceipt.gasUsed}`)
+    const tran = await blobUploader.sendTx(tx, blobs);
+    console.log(tran.hash);
+    const txReceipt = await tran.wait();
+    console.log(`blockNumber:${txReceipt.blockNumber} gasUsed:${txReceipt.gasUsed}`)
     return txReceipt;
 }
 
